0bba7c538093f71dbad28c5c60f19c64d0ee68ef
bodyaka <bogdankalysh20@gmail.com>
2024-01-13 16:12:57 +0200

    Added semiBald span to permission name in explanation popup for permission request. Applied portrait only orientation to main activity.

--- app/src/main/java/com/bodyaka/imgnator/PermissionRequestDialogFragment.kt
+++ app/src/main/java/com/bodyaka/imgnator/PermissionRequestDialogFragment.kt
@@ -3,9 +3,15 @@ package com.bodyaka.imgnator
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.DialogFragment
 
@@ -30,6 +36,7 @@ class PermissionRequestDialogFragment : DialogFragment() {
             dialogView.findViewById<AppCompatButton>(R.id.not_now_button).setOnClickListener {
                 dismiss()
             }
+            setPermissionNameSemiBold(dialogView)
 
             dialog
         } ?: throw IllegalStateException("Activity cannot be null")
@@ -39,5 +46,21 @@ class PermissionRequestDialogFragment : DialogFragment() {
         val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
         intent.data = Uri.fromParts("package", activity?.packageName, null)
         startActivity(intent)
+    }
+
+    private fun setPermissionNameSemiBold(dialogView: View) {
+        val textView = dialogView.findViewById<TextView>(R.id.explanation_text)
+        val ss = SpannableString(textView.text)
+
+        val textToBald = resources.getString(R.string.permission_name)
+
+        val startIndex = ss.indexOf(textToBald)
+        val endIndex = startIndex + textToBald.length
+
+        val boldSpan = StyleSpan(Typeface.BOLD)
+
+        ss.setSpan(boldSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textView.text = ss
     }
 }
